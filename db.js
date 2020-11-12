8d1dd0cdcefdfc0da0ff5de4c533acc6e90dfe1e
Jordan Deveau <jordandeveau@Jordans-iMac.local>
2020-11-11 20:48:18 -0500

    Set up heroku's postgres connection string

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require('pg')
 require('dotenv').config()
 
-const db = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'yelp',
-  password: process.env.DATABASE_PASSWORD,
-  port: 5432
+const connectionString = process.env.DATABASE_URL
+
+const client = new Client({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 
-module.exports = db
+client.connect()
+
+// const db = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'yelp',
+//   password: process.env.DATABASE_PASSWORD,
+//   port: 5432
+// })
+
+module.exports = client
